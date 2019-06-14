fdf31a403dadcfb23fef645d355498b148643108
Paul Tyng <paul@paultyng.net>
2019-06-14 09:53:01 -0400

    Add some utility functions

--- generator/util.go
+++ generator/util.go
@@ -20,3 +20,41 @@ func isPointer(t types.Type) bool {
 	}
 	return false
 }
+
+func unwrapSlice(v types.Type) *types.Slice {
+	if v == nil {
+		return nil
+	}
+	var unwrap func(v types.Type) *types.Slice
+	unwrap = func(v types.Type) *types.Slice {
+		switch v := v.(type) {
+		case *types.Pointer:
+			return unwrap(v.Elem())
+		case *types.Named:
+			return unwrap(v.Underlying())
+		case *types.Slice:
+			return v
+		}
+		return nil
+	}
+	return unwrap(v)
+}
+
+func unwrapStruct(v types.Type) *types.Struct {
+	if v == nil {
+		return nil
+	}
+	var unwrap func(v types.Type) *types.Struct
+	unwrap = func(v types.Type) *types.Struct {
+		switch v := v.(type) {
+		case *types.Pointer:
+			return unwrap(v.Elem())
+		case *types.Named:
+			return unwrap(v.Underlying())
+		case *types.Struct:
+			return v
+		}
+		return nil
+	}
+	return unwrap(v)
+}
